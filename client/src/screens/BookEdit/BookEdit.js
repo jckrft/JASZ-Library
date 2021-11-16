321653d2cdea4172282045c3bb665570ef896bc5
sidneypaucar <89218131+sidneypaucar@users.noreply.github.com>
2021-11-16 10:19:19 -0500

    added bookedit

--- client/src/screens/BookEdit/BookEdit.js
+++ client/src/screens/BookEdit/BookEdit.js
@@ -1,9 +1,110 @@
-const BookEdit = () => {
-	return (
-		<div>
-			<h1>Test Edit</h1>
-		</div>
-	)
+import { useState, useEffect } from 'react'
+import './BookEdit.css'
+import { useParams, Redirect } from 'react-router-dom'
+import Layout from '../../components/Layout/Layout'
+import { getBook, updateBook } from '../../services/books'
+
+const BookEdit = (props) => {
+  const [book, setBook] = useState({
+    title: '',
+    author: '',
+    genre: '',
+    imgURL: '',
+    description: '',
+  })
+
+  const [isUpdated, setUpdated] = useState(false)
+  let { id } = useParams()
+
+  useEffect(() => {
+    const fetchBook = async () => {
+      const book = await getBook(id)
+      setBook(book)
+    }
+    fetchBook()
+  }, [id])
+
+  const handleChange = (event) => {
+    const { title, value } = event.target
+    setBook({
+      ...book,
+      [title]: value,
+    })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const updated = await updateBook(id, book)
+    setUpdated(updated)
+  }
+
+  if (isUpdated) {
+    return <Redirect to={`/books/${id}`} />
+  }
+
+  return (
+    <Layout>
+      <div className='book-edit'>
+        <div className='image-container'>
+          <img
+            className='edit-book-image'
+            src={book.imgURL}
+            alt={book.title}
+          />
+          <form onSubmit={handleSubmit}>
+            <input
+              className='edit-input-image-link'
+              placeholder='Image Link'
+              value={book.imgURL}
+              name='imgURL'
+              required
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+        <form className='edit-form' onSubmit={handleSubmit}>
+          <input
+            className='input-title'
+            placeholder='Title'
+            value={book.title}
+            name='title'
+            required
+            autoFocus
+            onChange={handleChange}
+          />
+          <input
+            className='input-author'
+            placeholder='Author'
+            value={book.author}
+            name='author'
+            required
+            onChange={handleChange}
+          />
+          <input
+            className='input-genre'
+            placeholder='Genre'
+            value={book.genre}
+            name='genre'
+            required
+            onChange={handleChange}
+          />
+          <textarea
+            className='textarea-description'
+            rows={10}
+            cols={78}
+            placeholder='Description'
+            value={book.description}
+            name='description'
+            required
+            onChange={handleChange}
+          />
+          <button type='submit' className='save-button'>
+            Save
+          </button>
+        </form>
+      </div>
+    </Layout>
+  )
 }
 
 export default BookEdit
