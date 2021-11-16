47e1ed2b500c127a5293d9e44b9682966dc9a946
jckrft <jckrft@gmail.com>
2021-11-15 21:29:29 -0500

    books screen book component

--- client/src/components/Book/Book.js
+++ client/src/components/Book/Book.js
@@ -1,1 +1,20 @@
-//comment
+import { Link } from 'react-router-dom';
+
+const Book = (props) => {
+  return (
+    <>
+      <Link className='book' to={`/books/${props._id}`}>
+        <img className='book-image'
+          src={props.imgURL}
+          alt={props.title}
+        />
+        <div className='book-title'>{props.title}</div>
+        <div className='book-description'>{props.description}</div>
+        <div className='book-author'>{props.author}</div>
+        <div className='book-genre'>{props.genre}</div>
+      </Link>
+    </>
+  )
+}
+
+export default Book
--- client/src/screens/Books/Books.js
+++ client/src/screens/Books/Books.js
@@ -1,8 +1,41 @@
+// import './screens/Books/Books.css';
+import Book from '../../components/Book/Book';
+import Layout from '../../components/Layout/Layout';
+
+import { useEffect, useState } from 'react';
+import { getBooks } from '../../services/books';
+
 const Books = () => {
-	return (
-		<div>
-			<h1>Test Books</h1>
-		</div>
+  const [books, setBooks] = useState(null)
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const allBooks = await getBooks()
+      setBooks(allBooks)
+    }
+    fetchBooks()
+  }, [])
+
+  if (!books) return <h1>loading...</h1>
+
+  return (
+    <Layout>
+      <div className='books'>
+        {books.map((book, index) => {
+          return (
+            <Book
+              _id={book._id}
+              title={book.title}
+              author={book.author}
+              genre={book.genre}
+              description={book.description}
+              imgURL={book.imgURL}
+            />
+          )
+        })}
+			
+      </div>
+      </Layout>
 	)
 }
 
