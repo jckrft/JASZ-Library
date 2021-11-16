2972aee9b0d729046493a1c5d887e5c19c2f0d36
sidneypaucar <89218131+sidneypaucar@users.noreply.github.com>
2021-11-16 15:04:18 -0500

    nav bar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,27 +5,28 @@ const authenticatedOptions = (
     <>
         <Link className="link" to="/add-book">Add Book</Link>
         <Link className="link" to="/sign-out">Sign Out</Link>
-        <Link className="link" to="/books">Books</Link>
     </>
 )
 const unauthenticatedOptions = (
     <>
         <Link className="link" to="/sign-up">Sign Up</Link>
         <Link className="link" to="/sign-in">Sign In</Link>
+    </>
+)
+const alwaysOptions = (
+    <>
         <Link className="link" to="/books">Books</Link>
     </>
 )
-
-const Nav = (props) => {
-        console.log(props.user)
-
+const Nav = ({ user }) => {
         return (
             <nav>
                 <div className="nav">
-                    <Link className="logo" to="/">JASZ Library</Link>
+                    <Link className="logo" to="/">BooksApp</Link>
                     <div className="links">
-                        {props.user && <div className="link welcome">Welcome to the library, {props.user.username ? props.user.username : props.user}</div>}
-                        {props.user !== "Guest" ? authenticatedOptions : unauthenticatedOptions}
+                        {user && <div className="link welcome">Welcome to the library, {user.username}</div>}
+                        {alwaysOptions}
+                        {user ? authenticatedOptions : unauthenticatedOptions}
                     </div>
                 </div>
             </nav>
