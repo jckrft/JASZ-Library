bd89412b66ddd7af9cb5376cdccaaf32c5f6f963
Alex Warton <alexnwarton@gmail.com>
2021-11-18 09:32:40 -0800

    added home search feature

--- client/src/screens/Home/Home.js
+++ client/src/screens/Home/Home.js
@@ -1,22 +1,29 @@
 import Layout from '../../components/Layout/Layout';
 import BookCards from '../../components/BookCards/BookCards';
 import Button from '@mui/material/Button';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import RandomBook from '../../components/RandomBook/RandomBook';
+
+
 import './Home.css';
 
 const Home = (props) => {
+	const {user, query, searchBooks} = props	
 
 	return (
-		<Layout user={ props.user ? (props.user.username ? props.user.username : props.user) : ""}>
+		<Layout user={ user ? (user.username ? user.username : user) : ""}>
 			<div className="home-page">
 				<h1 className="home-header">Welcome to the Library</h1>
 				<h2 className="recent-favorites">Recent Additions</h2>
 				<BookCards />
+				<input type="text" placeholder="Search Books" value={query} onChange={(ev) => searchBooks(ev.target.value)}/>
+				<Link to="/search-results">
+					<Button className='join-button' variant='contained'>Search</Button>
+				</Link>
       			<div className="hidden-container">
-	      			<h2 className={props.user ? "join-header-visible" : "join-header-hidden"}>Welcome Back!</h2>
-	      			<h2 className={props.user ? "join-header-hidden" : "join-header-visible"}>Interested in Contributing?</h2>
-			    	<Link className={props.user ? "sign-up-redirect-hidden" : "sign-up-redirect-visible"} to='/sign-up'>
+	      			<h2 className={user ? "join-header-visible" : "join-header-hidden"}>Welcome Back!</h2>
+	      			<h2 className={user ? "join-header-hidden" : "join-header-visible"}>Interested in Contributing?</h2>
+			    	<Link className={user ? "sign-up-redirect-hidden" : "sign-up-redirect-visible"} to='/sign-up'>
 			     		<Button className='join-button' variant='contained'>Join us!</Button>
 			    	</Link>
 		      	</div>
