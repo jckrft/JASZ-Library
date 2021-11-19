e5ca02ab609896c3010dcd1d9d0518df25810ffb
jckrft <jckrft@gmail.com>
2021-11-19 09:33:57 -0500

    book review component

---
+++ client/src/components/BookReviews/BookReviews.js
@@ -0,0 +1,33 @@
+import './BookReviews.css'
+
+const BookReviews = () => {
+  return (
+    <div className='book-reviews'>
+      <div className='book-1'>
+        <img
+          src='http://prodimage.images-bn.com/pimages/9781788289702_p0_v1_s1200x630.jpg'
+          alt='metamorphosis'
+          style={{
+            width: '200px',
+            height: '300px'
+          }}
+        />
+        <p className='book-1-p'>"The Metamorphosis is one of those rare books that completely sucks you in and shows you a world so distorted, dark, and yet so real that your mind learns the art of imagination."</p>
+      </div>
+
+      <div className='book-2'>
+        <img
+          src='https://images-na.ssl-images-amazon.com/images/I/91p-SU6ZFbL.jpg'
+          alt='the gone away world'
+          style={{
+            width: '200px',
+            height: '300px'
+          }}
+        />
+        <p className='book-2-p'>"The Gone-Away World is a long story that absolutely revels in its digressions and manic humor that relentlessly attacks the insanity of the military weapons mentality and the soul-destroying nature of corporations and conformity."</p>
+      </div>
+    </div>
+  )
+}
+
+export default BookReviews
